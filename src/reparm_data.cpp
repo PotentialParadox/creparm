ba544517b9e58f26360cf1436a4dc8a2ddd589b2
Dustin Tracy <dustin@Dustins-MacBook-Pro.local>
2016-04-10 08:57:05 -0400

    Runbest now runs two jobs.

--- src/reparm_data.cpp
+++ src/reparm_data.cpp
@@ -68,20 +68,35 @@ void reparm::ReparmData::CalculateHighLevel(){
 }
 
 void reparm::ReparmData::RunBest(){
-  reparm::GaussianInput input1{population_[0].GetInputs()[0]};
+  reparm::GaussianInput input_freq{population_[0].GetInputs()[0]};
+  reparm::GaussianInput input_es{population_[0].GetInputs()[0]};
   reparm::Header header1{"\%chk=best\n#P AM1(Input,Print) opt\n\nbest\n"};
-  input1.SetHeader(header1);
+  input_freq.SetHeader(header1);
   
-  reparm::GaussianInput input2{input1};
-  reparm::Header header2{"\%chk=best\n#P AM1(Input,Print) CIS(Singlets,NStates=" +
-                         std::to_string(GetReparmInput().GetNumberExcitedStates()) +
-                         ") pop(full) freq geom=Checkpoint\n\nbest1\n"};
+  /* Create an input file for caculating frequencies */
+  reparm::GaussianInput input2{input_freq};
+  reparm::Header header2{"\%chk=best\n#P AM1(Input,Print) freq geom=Checkpoint\n\nbest1\n"};
   input2.SetHeader(header2);
   input2.ClearCoordinates();
-  input1.Link(input2);
-  std::ofstream fout{"best.com"};
-  fout << input1.str();
+  input_freq.Link(input2);
+  std::ofstream fout{"best_freq.com"};
+  fout << input_freq.str();
+
+  /* Create an input file for caculating excited_states */
+  reparm::GaussianInput input3{input_freq};
+  reparm::Header header3{"\%chk=best\n#P AM1(Input,Print) CIS(Singlets,NStates=" +
+                         std::to_string(GetReparmInput().GetNumberExcitedStates()) +
+                         ") pop(full) geom=Checkpoint\n\nbest1\n"};
+  input3.SetHeader(header3);
+  input3.ClearCoordinates();
+  input_freq.Link(input3);
+  std::ofstream fout_es{"best_es.com"};
+  fout_es << input_freq.str();
+
+  
   fout.close();
-  std::string command{"g09 best.com best.log"};
+  std::string command{"g09 best_freq.com best_freq.log"};
+  systls::exec(command, 100000);
+  command = "g09 best_es.com best_es.log";
   systls::exec(command, 100000);
 }
